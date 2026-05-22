e4eb9f461bafc0856dfb0974edf80f486e4487d9
dxol03 <151935037+dxol03@users.noreply.github.com>
2026-05-22 22:26:37 +0900

    Update 3-1.c

--- code/3-1.c
+++ code/3-1.c
@@ -6,7 +6,7 @@ int main(int argc, const char* argv[]) {
     
     FILE* fp = fopen(argv[1], "r");
     if(fp == NULL) {
-        perror("file dose not exitst");
+        perror("file dose not exists");
         return 1;
     }
 
