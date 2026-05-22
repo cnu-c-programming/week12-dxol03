7c8bf8f7cd9ce0c627245f218d808e5cb0a5c020
dxol03 <151935037+dxol03@users.noreply.github.com>
2026-05-22 22:10:22 +0900

    Update 2-4.c

--- code/2-4.c
+++ code/2-4.c
@@ -5,6 +5,16 @@ int main(int argc, const char* argv[]) {
         return 0;
 
     FILE* fp = fopen(argv[1], "r");
+    int count[26] = {0};
+    int c;
+    while((c = fgetc(fp)) != EOF) {
+        if (c >= 'a' && c <= 'z') {
+                count[c-'a']++;
+        }
+    }
+    for(int i = 0; i < 26; i++) {
+        printf("%c: %d\n",'a'+i,count[i]);
+    }
 
 
 
